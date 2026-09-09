875c2f2aad089d8bcabee7764eac7d7eada32777
unknown <1107836071@u.icesi.edu.co>
2026-09-09 12:43:21 -0500

    feat: provision ECR repositories

--- environments/dev/dev.tfvars
+++ environments/dev/dev.tfvars
@@ -1,4 +1,7 @@
-aws_region = "us-east-1"
+aws_region  = "us-east-1"
+environment = "dev"
+
+network_name = "docket-dev-vpc"
 
 vpc_cidr = "10.10.0.0/16"
 
@@ -17,12 +20,52 @@ public_subnets = [
   "10.10.102.0/24"
 ]
 
+cluster_name       = "docket-dev"
 kubernetes_version = "1.35"
 
 eks_instance_types = [
   "t3.small"
 ]
 
+eks_min_size     = 1
+eks_max_size     = 2
+eks_desired_size = 1
+
 cluster_admin_user_arn = "arn:aws:iam::429418377318:user/JuanP"
 
 github_actions_role_arn = "arn:aws:iam::429418377318:role/GitHubActionsDocketInfra"
+
+project_name = "Sintratel-Docket"
+
+ecr_repositories = {
+
+  "docket/frontend" = {
+    image_tag_mutability = "IMMUTABLE"
+    scan_on_push         = true
+    encryption_type      = "AES256"
+  }
+
+  "docket/auth-api" = {
+    image_tag_mutability = "IMMUTABLE"
+    scan_on_push         = true
+    encryption_type      = "AES256"
+  }
+
+  "docket/users-api" = {
+    image_tag_mutability = "IMMUTABLE"
+    scan_on_push         = true
+    encryption_type      = "AES256"
+  }
+
+  "docket/todos-api" = {
+    image_tag_mutability = "IMMUTABLE"
+    scan_on_push         = true
+    encryption_type      = "AES256"
+  }
+
+  "docket/log-message-processor" = {
+    image_tag_mutability = "IMMUTABLE"
+    scan_on_push         = true
+    encryption_type      = "AES256"
+  }
+}
--- environments/dev/variables.tf
+++ environments/dev/variables.tf
@@ -1,36 +1,71 @@
 variable "aws_region" {
-  description = "AWS region"
+  description = "AWS region where the DEV infrastructure is deployed"
+  type        = string
+}
+
+variable "environment" {
+  description = "Environment name"
+  type        = string
+}
+
+variable "network_name" {
+  description = "Name of the VPC/network"
   type        = string
 }
 
 variable "vpc_cidr" {
-  description = "CIDR block for the DEV VPC"
+  description = "CIDR block for the VPC"
   type        = string
 }
 
 variable "azs" {
-  description = "Availability Zones"
+  description = "Availability Zones used by the environment"
   type        = list(string)
 }
 
 variable "private_subnets" {
-  description = "Private subnet CIDRs"
+  description = "CIDR blocks for private subnets"
   type        = list(string)
 }
 
 variable "public_subnets" {
-  description = "Public subnet CIDRs"
+  description = "CIDR blocks for public subnets"
   type        = list(string)
 }
 
+variable "cluster_name" {
+  description = "Name of the EKS cluster"
+  type        = string
+}
+
 variable "kubernetes_version" {
-  description = "Kubernetes version for EKS"
+  description = "Kubernetes version used by EKS"
   type        = string
 }
 
 variable "eks_instance_types" {
-  description = "EC2 instance types used by EKS"
+  description = "EC2 instance types used by the EKS managed node group"
   type        = list(string)
+}
+
+variable "eks_min_size" {
+  description = "Minimum number of EKS worker nodes"
+  type        = number
+}
+
+variable "eks_max_size" {
+  description = "Maximum number of EKS worker nodes"
+  type        = number
+}
+
+variable "eks_desired_size" {
+  description = "Desired number of EKS worker nodes"
+  type        = number
+}
+
+variable "cluster_admin_user_arn" {
+  description = "IAM user used for local EKS administration"
+  type        = string
 }
 
 variable "github_actions_role_arn" {
@@ -38,7 +73,17 @@ variable "github_actions_role_arn" {
   type        = string
 }
 
-variable "cluster_admin_user_arn" {
-  description = "IAM user used for local EKS administration"
+variable "project_name" {
+  description = "Project name"
   type        = string
+}
+
+variable "ecr_repositories" {
+  description = "ECR repositories used by the microservices"
+
+  type = map(object({
+    image_tag_mutability = string
+    scan_on_push         = bool
+    encryption_type      = string
+  }))
 }
